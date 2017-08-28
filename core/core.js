de3e063fc5a67016fb469e5e4915649168a2ab80
ᦖєgιт <support@legitsoulja.info>
2017-08-28 12:00:32 -0400

    Create core.js

---
+++ core/core.js
@@ -0,0 +1,25 @@
+"use strict";
+
+(function(win, doc, pc){
+
+  function emacheck() {
+      try { eval("var foo = (x)=>x+1"); }
+      catch (e) { return false; }
+      return true;
+  }
+  
+  if(!emacheck()) return alert("Sigh.. Please update your browser please!. I'm begging you.");
+
+  var canvas = getElementsByClassName('game')[0];
+  if(!canvas) return console.log("Failed to find canvas");
+  
+  var gz = new pc.Application(canvas, {});
+  
+  // gz.start();
+  
+  gz.setCanvasFillMode(pc.FILLMODE_FILL_WINDOW);
+  gz.setCanvasResolution(pc.RESOLUTION_AUTO);
+  
+  win.onresize = () => gz.resizeCanvas();
+
+})(window, document, pc);
