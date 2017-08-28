15ad6cfb8cbc11983d06be891078766da5c5e391
ᦖєgιт <support@legitsoulja.info>
2017-08-28 12:02:21 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
     <title>G|Z</title>
     <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0' />
     <style>* {margin:0} body, html {width: 100%; height:100%; overflow: hidden;}</style>
-    <script type='text/javascript' src='pc.js'></script>
+    <script type='text/javascript' src='assets/js/pc.js'></script>
   </head>
   <body>
     <canvas class='game'></canvas>
-    <script type='text/javascript' src='core.js'></script>
+    <script type='text/javascript' src='core/core.js'></script>
   </body>
 </html>
